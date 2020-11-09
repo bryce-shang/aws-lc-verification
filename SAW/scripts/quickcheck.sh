0817e97c21ec3045d9e3b136c72cd60f8f04c3d7
Bryce Shang <shanggu@amazon.com>
2020-11-08 17:49:41 -0800

    Replace 512-384-quickcheck with all.

--- SAW/scripts/quickcheck.sh
+++ SAW/scripts/quickcheck.sh
@@ -6,7 +6,7 @@
 set -e
 
 saw proof/SHA256/SHA256.saw
-saw proof/SHA512/verify-SHA512-384-quickcheck.saw
+saw proof/SHA512/verify-SHA512-384-all.saw
 saw proof/SHA512/verify-SHA512-512-quickcheck.saw
 saw proof/HMAC/verify-HMAC-SHA384-quickcheck.saw
 saw proof/AES/verify-AES-GCM.saw
