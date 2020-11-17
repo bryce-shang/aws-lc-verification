8956762c5c5e38c783f703cdec58ac1fea9e7b00
Bryce Shang <shanggu@amazon.com>
2020-11-16 18:34:00 -0800

    Split HMAC.saw to avoid duplicate checks.

--- SAW/proof/HMAC/HMAC-check-entrypoint.go
+++ SAW/proof/HMAC/HMAC-check-entrypoint.go
@@ -13,6 +13,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // A utility function to terminate this program when err exists.
@@ -22,23 +23,24 @@ func checkError(e error) {
 	}
 }
 
-// A function to create and run "verify-HMAC-SHA384.saw".
-func createAndRunSawScript(is_quickcheck_enabled bool) {
-	log.Printf("Start creating saw script with quick_check=[%t].", is_quickcheck_enabled)
-	// Create a new saw script for the target_num.
-	file_name := fmt.Sprint("verify-SHA512-384.saw")
+// A function to create and run saw script.
+func createAndRunSawScript(path_to_template string, placeholder_key string, value int, wg *sync.WaitGroup) {
+	log.Printf("Start creating saw script for target value %s based on template %s.", value, path_to_template)
+	// Create a new saw script.
+	file_name := fmt.Sprint(value, ".saw")
 	file, err := os.Create(file_name)
 	checkError(err)
 	// Read file content of verification template.
-	content, err := ioutil.ReadFile("verify-HMAC-SHA384-template.txt")
+	content, err := ioutil.ReadFile(path_to_template)
 	checkError(err)
 	verification_template := string(content)
 	// Replace some placeholders of the file content with target values.
-	text := strings.Replace(verification_template, "HMAC_QUICK_CEHCK_PLACEHOLDER", strconv.FormatBool(is_quickcheck_enabled), 1)
+	text := strings.Replace(verification_template, placeholder_key, strconv.Itoa(value), 1)
 	defer file.Close()
 	file.WriteString(text)
 	defer os.Remove(file_name)
 	// Run saw script.
+	defer wg.Done()
 	runSawScript(file_name)
 }
 
@@ -56,9 +58,29 @@ func main() {
 	log.Printf("Started HMAC check.")
 	// When 'HMAC_SELECTCHECK' is undefined, quickcheck is executed.
 	env_var := os.Getenv("HMAC_SELECTCHECK")
-	is_quickcheck_enabled := len(env_var) == 0
+	if len(env_var) == 0 {
+		runSawScript("verify-HMAC-SHA384-quickcheck.saw")
+		return
+	}
 
-	// Generate saw scripts based on above verification template and target num ranges.
-	createAndRunSawScript(is_quickcheck_enabled)
+	// When 'HMAC_SELECTCHECK' is defined, run 'HMAC-Init-ex' with diff parameters concurrently.
+	var wg sync.WaitGroup
+	for num := 0; num <= 129; num++ {
+		wg.Add(1)
+		saw_template := "verify-HMAC-Init-ex-selectcheck-template.txt"
+		placeholder_name := "HMAC_TARGET_KEY_LEN_PLACEHOLDER"
+		go createAndRunSawScript(saw_template, placeholder_name, num, &wg)
+	}
+	wg.Wait()
+
+	// When 'HMAC_SELECTCHECK' is defined, run 'HMAC-Final' with diff parameters concurrently.
+	for num := 0; num <= 127; num++ {
+		wg.Add(1)
+		saw_template := "verify-HMAC-Final-selectcheck-template.txt"
+		placeholder_name := "HMAC_TARGET_NUM_PLACEHOLDER"
+		go createAndRunSawScript(saw_template, placeholder_name, num, &wg)
+	}
+
+	wg.Wait()
 	log.Printf("Completed HMAC check.")
 }
