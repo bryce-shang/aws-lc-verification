12f39e1fb86040ff1d765d64526d6f837d1abcb4
Bryce Shang <shanggu@amazon.com>
2020-11-22 23:02:10 -0800

    Run AES check with diff gcm_len.

--- SAW/proof/AES/AES-check-entrypoint.go
+++ SAW/proof/AES/AES-check-entrypoint.go
@@ -11,17 +11,29 @@ import (
 	"os"
 )
 
+const aes_process_limit int = 20
+
 func main() {
 	log.Printf("Started AES check.")
 	// When 'AES_SELECTCHECK' is undefined, quickcheck is executed.
 	env_var := os.Getenv("AES_SELECTCHECK")
 	if len(env_var) == 0 {
-		utility.RunSawScript("verify-AES-GCM.saw")
+		utility.RunSawScript("verify-AES-GCM-quickcheck.saw")
 		return
 	}
 
-	// When 'AES_SELECTCHECK' is defined, selectcheck is executed.
-	utility.RunSawScript("verify-HMAC-SHA384-selectcheck.saw")
+	// Generate saw scripts based on above verification template and target gcm len.
+	var wg sync.WaitGroup
+	process_count := 0
+	for gcm_len := 0; gcm_len <= 32; gcm_len++ {
+		wg.Add(1)
+		saw_template := "verify-AES-GCM-selectcheck.txt"
+		placeholder_name := "GCM_LEN_PLACEHOLDER"
+		go utility.CreateAndRunSawScript(saw_template, placeholder_name, gcm_len, &wg)
+		utility.Wait(&process_count, aes_process_limit, &wg)
+	}
+
+	wg.Wait()
 
 	log.Printf("Completed AES check.")
 }
