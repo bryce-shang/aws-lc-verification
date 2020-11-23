bc2c28d77155c3c7a7deb84574174e4b7410787a
Bryce Shang <shanggu@amazon.com>
2020-11-23 12:29:05 -0800

    Only enable AES check.

--- SAW/scripts/entrypoint_check.sh
+++ SAW/scripts/entrypoint_check.sh
@@ -7,7 +7,7 @@ set -e
 
 PATH=/lc/bin:/go/bin:$PATH
 
-./scripts/build_x86.sh
-./scripts/build_llvm.sh
-./scripts/post_build.sh
+# ./scripts/build_x86.sh
+# ./scripts/build_llvm.sh
+# ./scripts/post_build.sh
 ./scripts/run_checks.sh
