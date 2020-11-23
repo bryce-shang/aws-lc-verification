e93d773e10295e02657727bd1fa2bb7006f2a499
Bryce Shang <shanggu@amazon.com>
2020-11-23 12:23:39 -0800

    Run AES saw with diff update and final len.

--- SAW/scripts/run_checks.sh
+++ SAW/scripts/run_checks.sh
@@ -9,8 +9,6 @@ saw proof/SHA256/SHA256.saw
 (cd proof/SHA512 && go run SHA512-384-check-entrypoint.go)
 saw proof/SHA512/verify-SHA512-512-quickcheck.saw
 (cd proof/HMAC && go run HMAC-check-entrypoint.go)
-# TODO: add select check flavors for AES-GCM and AES-KW
-saw proof/AES/verify-AES-GCM.saw
+(cd proof/AES && go run AES-GCM-check-entrypoint.go)
 saw proof/AES_KW/verify-AES_KW.saw
 saw proof/AES_KW/verify-AES_KWP.saw
-
