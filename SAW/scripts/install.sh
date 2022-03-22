4c502c164ebc06aef8d1b8877b322985a6f0b597
Bryce Shang <shanggu@amazon.com>
2022-03-22 10:02:09 -0700

    Install saw when new version is defined.

--- SAW/scripts/install.sh
+++ SAW/scripts/install.sh
@@ -7,7 +7,8 @@ set -ex
 
 Z3_URL='https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-ubuntu-16.04.zip'
 YICES_URL='https://yices.csl.sri.com/releases/2.6.2/yices-2.6.2-x86_64-pc-linux-gnu-static-gmp.tar.gz'
-SAW_URL='https://saw.galois.com/builds/nightly/saw-0.9.0.99-2022.03.17-Linux-x86_64.tar.gz'
+TARGET_SAW_VERSION='0.9.0.99'
+SAW_URL="https://saw.galois.com/builds/nightly/saw-${TARGET_SAW_VERSION}-2022.03.17-Linux-x86_64.tar.gz"
 
 mkdir -p bin deps
 
@@ -30,11 +31,24 @@ then
     cp deps/yices/*/bin/yices-smt2 bin/yices-smt2
 fi
 
-# fetch SAW
-if [ ! -f bin/saw ]
-then
+# fetch SAW when not exist.
+install_saw () {
+    rm -rf bin/saw
     mkdir -p deps/saw
     wget $SAW_URL -O deps/saw.tar.gz
     tar -x -f deps/saw.tar.gz --one-top-level=deps/saw
-    cp deps/saw/*/bin/saw bin/saw
+    cp -r deps/saw/*/bin/saw bin/saw
+}
+
+if [ ! -f bin/saw ]
+then
+    install_saw
+fi
+# fetch SAW when new version saw is defined.
+INSTALLED_VERSION="$(/bin/saw --version 2>&1)"
+if case ${INSTALLED_VERSION} in "${TARGET_SAW_VERSION}"*) true;; *) false;; esac; then
+    echo "${INSTALLED_VERSION} matches ${TARGET_SAW_VERSION}."
+else
+    echo "${INSTALLED_VERSION} does not match ${TARGET_SAW_VERSION}. Install ${TARGET_SAW_VERSION}."
+    install_saw
 fi
