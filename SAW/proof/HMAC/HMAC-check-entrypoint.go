7374eba7fbbd3286721f00489b6b0019ab953d38
Bryce Shang <shanggu@amazon.com>
2022-03-23 16:16:23 -0700

    Support HMAC proof select check split.

--- SAW/proof/HMAC/HMAC-check-entrypoint.go
+++ SAW/proof/HMAC/HMAC-check-entrypoint.go
@@ -19,9 +19,18 @@ func main() {
 		utility.RunSawScript("verify-HMAC-SHA384-quickcheck.saw")
 		return
 	}
-
 	// When 'HMAC_SELECTCHECK' is defined, selectcheck is executed.
-	utility.RunSawScript("verify-HMAC-SHA384-selectcheck.saw")
+	start_indx := utility.ParseSelectCheckRange("HMAC_SELECTCHECK_START_INDX", 0)
+	end_indx := utility.ParseSelectCheckRange("HMAC_SELECTCHECK_END_INDX", 127)
+	if !(start_indx >= 0 && end_indx < 128) {
+		log.Fatal("The HMAC select check range should be within [0, 127], but got [%s, %s]", start_indx, end_indx)
+	}
+	saw_template := "verify-HMAC-SHA384-selectcheck-template.txt"
+	placeholder_map := map[string]int{
+		"RANGE_START_INDX_PLACEHOLDER": start_indx,
+		"RANGE_END_INDX_PLACEHOLDER":   end_indx,
+	}
+	utility.CreateAndRunSawScript(saw_template, placeholder_map, nil)
 
 	log.Printf("Completed HMAC check.")
 }
